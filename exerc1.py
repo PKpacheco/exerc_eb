958c7fb46fb3a87ab25553c5bb5a0421e02704bf
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-07 15:02:38 -0300

    exercise 1

--- exerc1.py
+++ exerc1.py
@@ -2,29 +2,34 @@
 # Part 01 #
 ###########
 
-# Given a list of tuples of (name, score) return a list
-# of tuples (name, score) sorted by score, descending.
+'''
+Given a list of tuples of (name, score) return a list
+of tuples (name, score) sorted by score, descending.
+'''
 
-input_scoresx_01 = [
-   ('Mason', 85),
-   ('Henry', 92),
-   ('Jaime', 86),
-   ('Evan', 89),
-   ('Noelle', 81),
+input_scores_01 = [
+    ('Mason', 85),
+    ('Henry', 92),
+    ('Jaime', 86),
+    ('Evan', 89),
+    ('Noelle', 81),
 ]
 
 expected_output_01 = [
-   ('Henry', 92),
-   ('Evan', 89),
-   ('Jaime', 86),
-   ('Mason', 85),
-   ('Noelle', 81),
+    ('Henry', 92),
+    ('Evan', 89),
+    ('Jaime', 86),
+    ('Mason', 85),
+    ('Noelle', 81),
 ]
 
 
 def place_01(scores):
-   scores = []
-   return scores
+    scores = []
+    scores = sorted(input_scores_01, key=lambda score: score[1])
+    scores.reverse()
+    return scores
+
 
 print place_01(input_scores_01)
 print place_01(input_scores_01) == expected_output_01
