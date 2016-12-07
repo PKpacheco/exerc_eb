2f7c8314b766735762dbe28480bf2d3e8c97b210
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-07 14:08:49 -0300

    exercise

---
+++ exerc1.py
@@ -0,0 +1,30 @@
+###########
+# Part 01 #
+###########
+
+# Given a list of tuples of (name, score) return a list
+# of tuples (name, score) sorted by score, descending.
+
+input_scoresx_01 = [
+   ('Mason', 85),
+   ('Henry', 92),
+   ('Jaime', 86),
+   ('Evan', 89),
+   ('Noelle', 81),
+]
+
+expected_output_01 = [
+   ('Henry', 92),
+   ('Evan', 89),
+   ('Jaime', 86),
+   ('Mason', 85),
+   ('Noelle', 81),
+]
+
+
+def place_01(scores):
+   scores = []
+   return scores
+
+print place_01(input_scores_01)
+print place_01(input_scores_01) == expected_output_01
---
+++ exerc2.py
@@ -0,0 +1,32 @@
+
+
+###########
+# Part 02 #
+###########
+
+# Given a list of tuples of (name, score) return a list of tuples (name, placement)
+# in descending order. The higher the score, the better.
+
+input_scores_02 = [
+ ('Mason', 85),
+ ('Henry', 92),
+ ('Jaime', 86),
+ ('Evan', 89),
+ ('Noelle', 81),
+]
+
+expected_output_02 = [
+ ('Henry', 1),
+ ('Evan', 2),
+ ('Jaime', 3),
+ ('Mason', 4),
+ ('Noelle', 5),
+]
+
+
+def place_02(scores):
+   scores = []
+   return scores
+
+print place_02(input_scores_02)
+print place_02(input_scores_02) == expected_output_02
